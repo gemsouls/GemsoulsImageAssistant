d16cd88c4cfd6e453c4b6ab7d7aa2052893cb46b
PanQiWei <594557445@qq.com>
2021-12-13 19:24:26 +0800

    change: 1. 添加处理结果获取的逻辑

--- app.py
+++ app.py
@@ -7,9 +7,10 @@
 # @LastEditBy :
 
 import asyncio
+import threading
 from datetime import datetime
 from hashlib import md5
-from queue import Queue
+from queue import Queue, Empty
 import time
 from typing import *
 
@@ -52,6 +53,8 @@ class AppManager:
 
         self.is_running = True
 
+        threading.Thread(target=self._listen_queue_system_output, daemon=True).start()
+
     async def stop(self):
         print(f"[{datetime.now()}]->[{self.__class__.__name__}]->stopping...")
         if self.is_running:
@@ -65,6 +68,20 @@ class AppManager:
                 await asyncio.sleep(0.1)
             else:
                 return self.result_pool.pop(task_id)
+
+    def _listen_queue_system_output(self):
+        # listening queue_system_output and get finished task_message from it
+        while self.is_running:
+            try:
+                task_message: TaskMessage = self.queues.system_output_queue.get_nowait()
+            except Empty:
+                time.sleep(0.1)
+            else:
+                task_id = task_message.input_message.task_id
+                self.result_pool[task_id] = task_message
+                print(f"[{datetime.now()}]->[AssistantManager]->get task-{task_id} from system output queue "
+                      f"and added to result pool.")
+                time.sleep(0.1)
 
 
 # ===== init app ===== #
@@ -92,14 +109,14 @@ async def shutdown_event():
 @app.post("/image2text", response_model=TaskOutputMessage)
 async def image2text(input_message: TaskInputMessage):
     start = time.time()
-    task_id = md5(str(input_message.task_id) + str(datetime.now())).hexdigest()
+    task_id = md5((str(input_message.task_id) + str(datetime.now())).encode(encoding="utf-8")).hexdigest()
     input_message.task_id = task_id
     print(f"[{datetime.now()}]->[image2text]->get task-{task_id}.")
     task_message = TaskMessage(input_message=input_message)
     print(f"[{datetime.now()}]->[image2text]->adding task-{task_id} to execution process...")
     await app_manager.execution_process_handler.add_task(task_message)
     print(f"[{datetime.now()}]->[image2text]->waiting task-{task_id} to be processed...")
-    await app_manager.wait_task_done(task_id)
+    task_message = await app_manager.wait_task_done(task_id)
     end = time.time()
     print(f"[{datetime.now()}]->[image2text]->task-{task_id} process finished, using {end-start:.4f}s")
     return task_message.output_message
