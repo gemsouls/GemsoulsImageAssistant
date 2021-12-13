8df9e88653697816ffe8af5ad15edaf04455da13
PanQiWei <594557445@qq.com>
2021-12-13 19:24:39 +0800

    change: 1. 完善load_test逻辑

--- gia_test/performance_test/load_test.py
+++ gia_test/performance_test/load_test.py
@@ -12,6 +12,8 @@ import asyncio
 import datetime
 import os
 from os.path import join, dirname, abspath, exists
+import random
+import re
 import requests
 import sys
 import time
@@ -24,14 +26,24 @@ ROOT = dirname(dirname(dirname(abspath(__file__))))
 sys.path.insert(0, ROOT)
 
 
+def get_fb_log_images():
+    images = []
+    for root, ds, fs in os.walk(join(ROOT, "gia_test/test_src/images/fb_log_images_211208")):
+        for f in fs:
+            if re.match(r".*\.png", f):
+                fullname = os.path.join(root, f)
+                images.append(fullname)
+    return images
+
+
 async def send_messages_concurrently(url: str, num_messages: int = 10):
-    async def send_task(task_id, session):
+    async def send_task(task_id, img_path, session):
         start_time = datetime.datetime.now()
         async with session.post(
-            os.path.join(url, "propose"),
+            url,
             json={
                 "task_id": task_id,
-                "image": "",
+                "image_url": img_path,
             },
             headers={'content-type': 'application/json'}
         ) as response:
@@ -40,20 +52,22 @@ async def send_messages_concurrently(url: str, num_messages: int = 10):
         print(f"task-{task_id} using {(finish_time-start_time).total_seconds(): .4f}s")
         print(res)
 
+    image_paths = random.sample(get_fb_log_images(), num_messages)
+
     async with aiohttp.ClientSession() as session:
         await asyncio.gather(
-            *[send_task(task_id, session) for task_id in range(num_messages)]
+            *[send_task(task_id, img_path, session) for task_id, img_path in enumerate(image_paths)]
         )
 
 
 def send_message_every_n_seconds(url: str, num_messages: int = 10, n: float = 0.2):
-    def post_message(post_id):
+    def post_message(post_id, img_path):
         start_time = datetime.datetime.now()
         response = requests.post(
-            os.path.join(url, "propose"),
+            url,
             json={
                 "task_id": task_id,
-                "image": "",
+                "image_url": img_path,
             },
             verify=False,
             headers={'content-type': 'application/json'}
@@ -64,12 +78,61 @@ def send_message_every_n_seconds(url: str, num_messages: int = 10, n: float = 0.
         print(res)
 
     threads = []
-
-    for task_id in range(num_messages):
-        t = threading.Thread(target=post_message, args=(task_id,), daemon=True)
+    image_paths = get_fb_log_images()
+    for task_id, img_path in enumerate(random.sample(image_paths, num_messages)):
+        t = threading.Thread(target=post_message, args=(task_id, img_path), daemon=True)
         t.start()
         threads.append(t)
         time.sleep(n)
 
     for t in threads:
         t.join()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--host",
+        type=str,
+        default="0.0.0.0"
+    )
+    parser.add_argument(
+        "--port",
+        type=str,
+        default="6006"
+    )
+    parser.add_argument(
+        "--route",
+        type=str,
+        default="image2text"
+    )
+    parser.add_argument(
+        "--msg_num",
+        type=int,
+        default=10
+    )
+    parser.add_argument(
+        "--interval",
+        type=float,
+        default=0.1
+    )
+    parser.add_argument(
+        "--test_sequentially",
+        action="store_true"
+    )
+    parser.add_argument(
+        "--test_concurrently",
+        action="store_true"
+    )
+    args = parser.parse_args()
+    url = "http://" + args.host + ":" + args.port + f"/{args.route}"
+
+    if args.test_sequentially:
+        send_message_every_n_seconds(
+            url, args.msg_num, args.interval
+        )
+
+    if args.test_concurrently:
+        send_messages_concurrently(
+            url, args.msg_num
+        )
